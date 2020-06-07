23a8257aa45cb2ed33c59ca9e6964c3a3fd365a1
Ofek Shochat <o@shoch.at>
2020-06-07 07:15:32 +0200

    top

--- contactme.php
+++ contactme.php
@@ -2,11 +2,12 @@
 
 if(isset($_POST['submit'])) {
     $name = $_POST['name'];
-    $mailfrom = $_POST['email'];
+    $email = $_POST['email'];
     $message = $_POST['message'];
 
+    $mailfrom = "o@shoch.at"
     $mailto = "o@shoch.at";
-    $headers = "From (website): ".$mailfrom;
+    $headers = "From (website): ".$email;
     $txt = "You have received an email from ".$name.".\n\n".$message;
 
     mail($mailto, $txt, $headers);
