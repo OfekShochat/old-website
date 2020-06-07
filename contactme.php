1da392251525697947af399b9ad050618be90dcb
Ofek Shochat <o@shoch.at>
2020-06-07 06:29:23 +0200

    send emails

---
+++ contactme.php
@@ -0,0 +1,14 @@
+<?php
+
+if(isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailfrom = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mailto = "o@shoch.at";
+    $headers = "From (website): ".$mailfrom;
+    $txt = "You have received an email from ".$name.".\n\n".$message;
+
+    mail($mailto, $txt, $headers);
+    header("Location: contactme.php?mailsent");
+}
