d610476d75b6bf62467b73d9742c26087209c511
Ofek Shochat <o@shoch.at>
2020-06-09 12:27:57 +0200

    start of login

--- scripts.js
+++ scripts.js
@@ -63,3 +63,15 @@ function scrollFunction() {
 function changeimg(imgsource, object) {
     object.style.backgroundImage="url(" + imgsource + ")";
 }
+
+Object.defineProperty(String.prototype, 'hashCode', {
+    value: function() {
+      var hash = 0, i, chr;
+      for (i = 0; i < this.length; i++) {
+        chr   = this.charCodeAt(i);
+        hash  = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+      }
+      return hash;
+    }
+  });
